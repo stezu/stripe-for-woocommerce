5ba53980cd6c896ba73011b64740d523a3d36dbc
Stephen Zuniga <stephen.zuniga001@gmail.com>
2014-08-01 11:30:39 -0400

    Add some tabs
    Take some pride in your work.

--- woocommerce-stripe.php
+++ woocommerce-stripe.php
@@ -29,11 +29,11 @@ class WooCommerce_Stripe {
 		$this->settings = get_option( 'woocommerce_wc_stripe' . '_settings', array() );
 
 		// Add default values for fresh installs
-		$this->settings['testmode'] = isset( $this->settings['testmode'] ) ? $this->settings['testmode'] : 'yes';
-		$this->settings['test_publishable_key'] = isset( $this->settings['test_publishable_key'] ) ? $this->settings['test_publishable_key'] : '';
-		$this->settings['test_secret_key'] = isset( $this->settings['test_secret_key'] ) ? $this->settings['test_secret_key'] : '';
-		$this->settings['live_publishable_key'] = isset( $this->settings['live_publishable_key'] ) ? $this->settings['live_publishable_key'] : '';
-		$this->settings['live_secret_key'] = isset( $this->settings['live_secret_key'] ) ? $this->settings['live_secret_key'] : '';
+		$this->settings['testmode']					= isset( $this->settings['testmode'] ) ? $this->settings['testmode'] : 'yes';
+		$this->settings['test_publishable_key']		= isset( $this->settings['test_publishable_key'] ) ? $this->settings['test_publishable_key'] : '';
+		$this->settings['test_secret_key']			= isset( $this->settings['test_secret_key'] ) ? $this->settings['test_secret_key'] : '';
+		$this->settings['live_publishable_key']		= isset( $this->settings['live_publishable_key'] ) ? $this->settings['live_publishable_key'] : '';
+		$this->settings['live_secret_key']			= isset( $this->settings['live_secret_key'] ) ? $this->settings['live_secret_key'] : '';
 
 		// API Info
 		$this->settings['api_endpoint']				= 'https://api.stripe.com/';
