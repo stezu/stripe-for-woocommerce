cdad3b0708f4e95147652a8f7bcd4f38694b4b86
Stephen Zuniga <stephen.zuniga001@gmail.com>
2014-10-28 15:40:49 -0400

    remove spacing

--- classes/class-s4wc_db.php
+++ classes/class-s4wc_db.php
@@ -35,68 +35,69 @@ class S4WC_DB {
         //  'default_card'  => 'card_104FP72XTgyB3Fd3Pw9jM2Xh'
         // );
 
-        if ( isset( $customer_data ) ) {
+        if ( ! isset( $customer_data ) ) {
+            return;
+        }
 
-            // Set variables related to the form fields we're updating
-            $customer_id = isset( $customer_data['customer_id'] ) ? $customer_data['customer_id'] : null;
+        // Set variables related to the form fields we're updating
+        $customer_id = isset( $customer_data['customer_id'] ) ? $customer_data['customer_id'] : null;
 
+        if ( isset( $customer_data['card'] ) ) {
+            $card_id        = isset( $customer_data['card']['id'] ) ? $customer_data['card']['id'] : null;
+            $card_last4     = isset( $customer_data['card']['last4'] ) ? $customer_data['card']['last4'] : null;
+            $card_brand     = isset( $customer_data['card']['brand'] ) ? $customer_data['card']['brand'] : null;
+            $card_exp_month = isset( $customer_data['card']['exp_month'] ) ? $customer_data['card']['exp_month'] : null;
+            $card_exp_year  = isset( $customer_data['card']['exp_year'] ) ? $customer_data['card']['exp_year'] : null;
+        }
+
+        $default_card = isset( $customer_data['default_card'] ) ? $customer_data['default_card'] : null;
+
+        // Grab the current object out of the database and return a useable array
+        $currentObject = maybe_unserialize( get_user_meta( $user_id, $s4wc->settings['stripe_db_location'], true ) );
+
+        // If there is an exising object, append values
+        if ( $currentObject ) {
+            $newObject = $currentObject;
+
+            // Add a new card to the object
             if ( isset( $customer_data['card'] ) ) {
-                $card_id        = isset( $customer_data['card']['id'] ) ? $customer_data['card']['id'] : null;
-                $card_last4     = isset( $customer_data['card']['last4'] ) ? $customer_data['card']['last4'] : null;
-                $card_brand     = isset( $customer_data['card']['brand'] ) ? $customer_data['card']['brand'] : null;
-                $card_exp_month = isset( $customer_data['card']['exp_month'] ) ? $customer_data['card']['exp_month'] : null;
-                $card_exp_year  = isset( $customer_data['card']['exp_year'] ) ? $customer_data['card']['exp_year'] : null;
+                $newObject['cards'][] = array(
+                    'id'        => $card_id,
+                    'last4'     => $card_last4,
+                    'brand'     => $card_brand,
+                    'exp_month' => $card_exp_month,
+                    'exp_year'  => $card_exp_year
+                );
             }
 
-            $default_card = isset( $customer_data['default_card'] ) ? $customer_data['default_card'] : null;
-
-            // Grab the current object out of the database and return a useable array
-            $currentObject = maybe_unserialize( get_user_meta( $user_id, $s4wc->settings['stripe_db_location'], true ) );
-
-            // If there is an exising object, append values
-            if ( $currentObject ) {
-                $newObject = $currentObject;
-
-                // Add a new card to the object
-                if ( isset( $customer_data['card'] ) ) {
-                    $newObject['cards'][] = array(
-                        'id'        => $card_id,
-                        'last4'     => $card_last4,
-                        'brand'     => $card_brand,
-                        'exp_month' => $card_exp_month,
-                        'exp_year'  => $card_exp_year
-                    );
-                }
-
-                // Reference a new default card
-                if ( isset( $customer_data['default_card'] ) ) {
-                    $newObject['default_card'] = $default_card;
-                }
-            }
-
-            // Otherwise, create a new object
-            else {
-                $newObject = array();
-
-                $newObject['customer_id']   = $customer_id;
-                $newObject['cards']         = array();
-
-                // Add a new card to the object
-                if ( isset( $customer_data['card'] ) ) {
-                    $newObject['cards'][]       = array(
-                        'id'        => $card_id,
-                        'last4'     => $card_last4,
-                        'brand'     => $card_brand,
-                        'exp_month' => $card_exp_month,
-                        'exp_year'  => $card_exp_year
-                    );
-                }
+            // Reference a new default card
+            if ( isset( $customer_data['default_card'] ) ) {
                 $newObject['default_card'] = $default_card;
             }
-
-            // Add to the database
-            return update_user_meta( $user_id, $s4wc->settings['stripe_db_location'], $newObject );
         }
+
+        // Otherwise, create a new object
+        else {
+            $newObject = array();
+
+            $newObject['customer_id']   = $customer_id;
+            $newObject['cards']         = array();
+
+            // Add a new card to the object
+            if ( isset( $customer_data['card'] ) ) {
+                $newObject['cards'][]       = array(
+                    'id'        => $card_id,
+                    'last4'     => $card_last4,
+                    'brand'     => $card_brand,
+                    'exp_month' => $card_exp_month,
+                    'exp_year'  => $card_exp_year
+                );
+            }
+            $newObject['default_card'] = $default_card;
+        }
+
+        // Add to the database
+        return update_user_meta( $user_id, $s4wc->settings['stripe_db_location'], $newObject );
     }
 
     /**
