eba533a7cf4b92bb3c843a22851acd715e156441
Thomas Schmierer <thomas@plantminer.com.au>
2015-06-17 17:26:39 +1000

    Accidently committed a test solution, fixing it

--- classes/class-s4wc_gateway.php
+++ classes/class-s4wc_gateway.php
@@ -797,7 +797,7 @@ class S4WC_Gateway extends WC_Payment_Gateway {
         $new_card = FALSE;
         if($this->form_data['chosen_card'] === 'new') { $new_card = TRUE;}
         // Make sure we only create customers if a user is logged in
-        if (is_user_logged_in() && $this->settings['saved_cards'] === 'yes' && (($this->form_data['chosen_card'] === 'new' && $this->form_data['save_card'] === TRUE) || $this->form_data['chosen_card'] !== 'new')) {
+        if (is_user_logged_in() && $this->settings['saved_cards'] === 'yes' && (($new_card === TRUE && $this->form_data['save_card'] === TRUE) || $this->form_data['chosen_card'] !== 'new')) {
 
             // Add a customer or retrieve an existing one
             $customer = $this->get_customer();
